737ad32bf1348e6cf3f6f409e23d08522569dcf6
orangehat <jessdhines@gmail.com>
2021-04-23 12:06:26 -0400

    04-group-inputs: add Oxford comma
    When there are 3 or more items in a list, the second-to last item (before the word 'and') should be followed by a comma:
    "Let's go to eat George, Betty, and Martha!" (meaning "you three, let's go eat!")
    vs
    "Let's go to eat George, Betty and Martha!" (meaning, "Betty and Martha, let's eat George")

--- site/content/tutorial/06-bindings/04-group-inputs/app-a/App.svelte
+++ site/content/tutorial/06-bindings/04-group-inputs/app-a/App.svelte
@@ -2,45 +2,43 @@
 	let scoops = 1;
 	let flavours = ['Mint choc chip'];
 
+	let menu = [
+		'Cookies and cream',
+		'Mint choc chip',
+		'Raspberry ripple'
+	];
+
 	function join(flavours) {
 		if (flavours.length === 1) return flavours[0];
-		return `${flavours.slice(0, -1).join(', ')} and ${flavours[flavours.length - 1]}`;
+		return `${flavours.slice(0, -1).join(', ')}, and ${flavours[flavours.length - 1]}`;
 	}
 </script>
 
 <h2>Size</h2>
 
 <label>
-	<input type=radio group={scoops} value={1}>
+	<input type=radio bind:group={scoops} value={1}>
 	One scoop
 </label>
 
 <label>
-	<input type=radio group={scoops} value={2}>
+	<input type=radio bind:group={scoops} value={2}>
 	Two scoops
 </label>
 
 <label>
-	<input type=radio group={scoops} value={3}>
+	<input type=radio bind:group={scoops} value={3}>
 	Three scoops
 </label>
 
 <h2>Flavours</h2>
 
-<label>
-	<input type=checkbox group={flavours} value="Cookies and cream">
-	Cookies and cream
-</label>
-
-<label>
-	<input type=checkbox group={flavours} value="Mint choc chip">
-	Mint choc chip
-</label>
-
-<label>
-	<input type=checkbox group={flavours} value="Raspberry ripple">
-	Raspberry ripple
-</label>
+{#each menu as flavour}
+	<label>
+		<input type=checkbox bind:group={flavours} value={flavour}>
+		{flavour}
+	</label>
+{/each}
 
 {#if flavours.length === 0}
 	<p>Please select at least one flavour</p>
